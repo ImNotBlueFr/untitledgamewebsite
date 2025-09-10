d84efd9ad42a1c3a1be5d13b4f682bfc0f6d68f2
Blue <imnotbluefrfr@gmail.com>
2025-09-10 18:42:14 -0500

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -18,40 +18,10 @@ var apps = [
       'image': 'https://static.vecteezy.com/system/resources/previews/006/892/625/original/discord-logo-icon-editorial-free-vector.jpg'
     },
     {
-      "id": "telegram",
-      "title": "Telegram",
-      "url": "https://web.telegram.org/a/",
-      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png'
-    }, 
-    {
-      "id": "guilded",
-      "title": "Guilded",
-      "url": "https://www.guilded.gg/",
-      'image': 'https://yt3.googleusercontent.com/cPlwwgZkQQgHRLAg7jvJG4yKGLDId6NH7H_S87TCoEV_dbsF9Me_4YSBL5DyMsndx6hzoZxHUw=s176-c-k-c0x00ffffff-no-rj'
-    },
-    {
-      "id": "geforce",
-      "title": "GeForce NOW (may not work)",
-      "url": "https://play.geforcenow.com",
-      'image': 'https://raw.githubusercontent.com/proudparrot2/cdn/main/8z9zeDIT_400x400-removebg-preview.png'
-    },
-    {
-      'id': 'vscode',
-      'title': 'Visual Studio Code',
-      'url': 'https://vscode.dev/',
-      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png'
-    },
-    {
       "id": "tiktok",
       "title": "TikTok",
       "url": "https://tiktok.com",
       'image': 'https://sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png'
-    },
-    {
-      "id": "reddit",
-      "title": "Reddit",
-      "url": "https://reddit.com",
-      'image': 'https://www.redditinc.com/assets/images/site/reddit-logo.png'
     },
     {
       "id": "youtube",
@@ -60,28 +30,10 @@ var apps = [
       'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/YouTube_social_red_square_%282017%29.svg/2048px-YouTube_social_red_square_%282017%29.svg.png'
     },
     {
-      "id": "twitter",
-      "title": "Twitter",
-      "url": "https://twitter.com",
-      'image': 'https://seeklogo.com/images/T/twitter-icon-square-logo-108D17D373-seeklogo.com.png'
-    },
-    {
       'id': 'spotify',
       'title': 'Spotify',
       'url': 'https://open.spotify.com/browse',
       'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png'
-    },
-    {
-      'id': 'chess',
-      'title': 'Chess.com',
-      'url': 'https://chess.com',
-      'image': 'https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpmeXx6V.png'
-    },
-    {
-      'id': 'movie-web',
-      'title': 'movie-web',
-      'url': 'http://movie-web.app/',
-      'image': 'https://github.com/movie-web/movie-web/blob/dev/public/android-chrome-512x512.png?raw=true'
     },
     {
       'id': 'coolmathgames',
@@ -90,33 +42,113 @@ var apps = [
       'image': 'https://docs.google.com/drawings/d/e/2PACX-1vR6y2GvcGu9PdrX9zzE24dhm24hClcNsdAmZjIgSUC8sFOahf6t7Yg6l_W8Rd4GduWOH_X02GHVFdBb/pub?w=512&h=512'
     },
     {
-      'id': 'win11',
-      'title': 'Windows 11 in React',
-      'url': 'https://win11.blueedge.me/',
-      "image": "https://cdn.upload.systems/uploads/lnysPmIS.png",
-      "description": "A remake of the Windows 11 UI in React.js"
+      'id': 'xboxcloudgaming',
+      'title': 'Xbox Cloud Gaming',
+      'url': 'https://www.xbox.com/en-us/play',
+      'image': 'https://cdn-1.webcatalog.io/catalog/xbox-cloud-gaming/xbox-cloud-gaming-icon-filled-256.png?v=1714776527567'
+    },
+     {
+      'id': 'facebook',
+      'title': 'FaceBook',
+      'url': 'https:///www.facebook.com',
+      'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkLcBrDHgOj0B_qrNTygXlcjOPlRfGOBqZrw&s'
     },
     {
-      'id': 'win10',
-      'title': 'Windows 10 in React',
-      'url': 'https://win-10.vercel.app/',
-      "image": "https://downlody.com/windows/files/image/Windows-10-icon.png",
-      "description": "Windows 10 recreation in React.js"
+      'id': 'instagram',
+      'title': 'Instagram',
+      'url': 'https://www.instagram.com',
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png'
+    },
+     {
+      'id': 'chatgpt',
+      'title': 'ChatGPT',
+      'url': 'https://chatgpt.com',
+      'image': 'https://static.vecteezy.com/system/resources/previews/021/059/827/non_2x/chatgpt-logo-chat-gpt-icon-on-white-background-free-vector.jpg'
     },
     {
-      'id': 'winxp',
-      'title': 'Windows XP in React',
-      'url': 'https://www.oonjaa.com/',
-      "image": "https://archive.org/services/img/1d9464dbff7a1630882d0e2090cb714c",
-      "description": "Windows XP recreation in React.js"
+      'id': 'snapchat',
+      'title': 'Snapchat',
+      'url': 'https://www.snapchat.com/?utm_source=GoogleSEM&utm_medium=PAIDUA&utm_campaign=Brand_SnapforWeb_US&utm_term=Web_Desktop&utm_content=RSA&gad_source=1&gclid=Cj0KCQjwlMfABhCWARIsADGXdy9qFlCnkSoVoNoYPTYaMAcc3CmjUYEK8M1cG4jst2AsoPA7H3YoHdkaAucrEALw_wcB&original_referrer=www.google.com',
+      'image': 'https://cdn-icons-png.freepik.com/256/15707/15707923.png?semt=ais_hybrid'
     },
     {
-      'id': 'gbaemulator',
-      'title': 'GBA Emulator',
-      'url': 'https://cattn.github.io/gba',
-      "image": "https://cattn.github.io/gba/pwa/apple-icon-180.png",
-      "description": "A Game Boy Advance retro emulator with over 3000 games, and other consoles as well"
-    }
+      'id': 'gauthai',
+      'title': 'Gauth AI',
+      'url': 'https://www.gauthmath.com/?utm_source=google&utm_medium=cpa_home&utm_campaign=22100242799&utm_term=math%20ai&utm_content=728344988825&gad_source=1&gclid=Cj0KCQjw8cHABhC-ARIsAJnY12ydKoJVtk6g3P6t2pHez_E9dad4JYQYtVMjmrK_DLPLMsRz2pJDmrMaApmBEALw_wcB',
+      'image': 'https://play-lh.googleusercontent.com/1CnI8ztueH5zyg2PLN1qEcPaL_j56Y48gEr2OKcD31ce799Qw91_U0QneDLmwYlCo3w'
+    },
+    {
+      'id': 'characterai',
+      'title': 'Character AI',
+      'url': 'character.ai',
+      'image': 'https://media.licdn.com/dms/image/v2/C560BAQFnYb3w5mQMKQ/company-logo_200_200/company-logo_200_200/0/1679554051094/character_ai_logo?e=2147483647&v=beta&t=CrD2N3TToQOyeAI6JSoHIqeA1NlgVA22iIWMTmQcdWc'
+    },
+    {
+      'id': 'rocketleague',
+      'title': 'Rocket League Website',
+      'url': 'https://www.rocketleague.com/en',
+      'image': 'https://cdn2.iconfinder.com/data/icons/popular-games-1/50/rocketleague_squircle-512.png'
+    },
+    {
+      'id': 'netlfix',
+      'title': 'Netflix',
+      'url': 'https://www.netflix.com',
+      'image': 'https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456'
+    },
+     {
+      'id': 'monkeytype',
+      'title': 'MonkeyType',
+      'url': 'https://www.monkeytype.com',
+      'image': 'https://images.sftcdn.net/images/t_app-icon-m/p/8d9031a7-6ae1-43e3-9353-3dbc7ee10bc9/4060554368/monkeytype-enhancer-logo'
+    },
+    {
+      'id': 'primevideo',
+      'title': 'Amazon Prime Video',
+      'url': 'https://www.amazon.com/Amazon-Video/b/?&node=2858778011&ref=dvm_MLP_ROWNA_US_1',
+      'image': 'https://store-images.s-microsoft.com/image/apps.37888.14618985536919905.3e754390-a812-43d7-87fc-335159cd867b.f9c3de38-8b93-42bb-a403-0374ccf355cd'
+    },
+    {
+      'id': 'twitch',
+      'title': 'Twitch',
+      'url': 'http://twitch.tv/',
+      'image': 'https://imgs.search.brave.com/K0NQUpWCdhpcS9uuSgNmgVOMgEw7lBijH3Di-vOfXig/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuaWNvbi1pY29u/cy5jb20vMzA0MS9Q/TkcvNTEyL3R3aXRj/aF9sb2dvX2ljb25f/MTg5MjQyLnBuZw'
+    },
+    {
+      'id': 'disneyplus',
+      'title': 'Disney+',
+      'url': 'https://www.disneyplus.com/',
+      'image': 'https://imgs.search.brave.com/OrGQMtwUsS26ogR5aqe3q7yCL5WT6afsacMaUq4toYQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzJiL2Zj/L2YxLzJiZmNmMWY1/M2I1ZDZmYzEwOTk4/NTA5MTUyMDExMzY4/LmpwZw'
+    },
+    {
+      'id': 'xboxcloudgaming',
+      'title': 'XBOX Cloud Gaming',
+      'url': 'https://www.xbox.com/en-us/play',
+      'image': 'https://imgs.search.brave.com/35ifVPQCXERSzcKLMWp8teXtLboL26jEaFnMeLVuBcE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzQ1LzIveGJveC1j/bG91ZC1nYW1pbmct/bG9nby1wbmdfc2Vl/a2xvZ28tNDU3MTcx/LnBuZw'
+    },
+    {
+      'id': 'paramountplus',
+      'title': 'Paramount Plus',
+      'url': 'https://www.paramountplus.com/',
+      'image': 'https://imgs.search.brave.com/WL26R7S7qY137540f7QWk2QhOFy-Hu5BYpcYj5agFjs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdHls/ZXMucmVkZGl0bWVk/aWEuY29tL3Q1XzJ6/b2s2YS9zdHlsZXMv/Y29tbXVuaXR5SWNv/bl91MXEwbzh4a2hm/NTYxLnBuZw'
+    },
+    {
+      'id': 'lookmovie2',
+      'title': 'LookMovie2',
+      'url': 'https://www.lookmovie2.to/',
+      'image': 'https://www.lookmovie2.to/images/logo1.webp'
+    },
+    {
+      'id': 'newgrounds',
+      'title': 'Newgrounds',
+      'url': 'https://www.newgrounds.com/',
+      'image': 'https://imgs.search.brave.com/wW31X0zcYltVqIELBcpxjo2hWh_Jp_-E-pnre1XQ1io/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvNWY1NDBhZDJm/NDVkOTRjOGVhMmZm/ZWE0MjgzZTg3YWQ2/YTM0Y2NiZjQ3MzQz/ODNjNmI1ZDlhZjBh/NjEwYWJkZC93d3cu/bmV3Z3JvdW5kcy5j/b20v'
+    },
+    {
+      'id': 'soundcloud',
+      'title': 'Soundcloud',
+      'url': 'https://soundcloud.com/discover',
+      'image': 'https://imgs.search.brave.com/cITFBqfwL8R8KcVrBVnr4mutx1MdwZ3SrxiSlMNye2E/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvMTA5ZDgyNGJl/NjljY2RjMmFkYWFi/YThkMjA5NjkxNTNh/YzBmMGI0YWIxNGE4/YTVhNjI3N2IwODQ0/ZTEyMDk4Yy9zb3Vu/ZGNsb3VkLmNvbS8'
+    },
   ]
   
   var customapps = JSON.parse(localStorage.getItem("customapps"))
