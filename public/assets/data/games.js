e099ac9fefe854d016e7038b6cfba98381330196
Blue <imnotbluefrfr@gmail.com>
2025-09-15 17:38:33 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -603,6 +603,14 @@ var games = [
   url: 'https://games.crazygames.com/en_US/10-minutes-till-dawn/index.html?ssrDevice=desktop&isNewUser=false&v=1.338',
   image: 'https://imgs.search.brave.com/Q4ILZacqBT0xZvLZp5o8qArgQBK5e25XE5AyBEkW-qc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aGF0/c25vdG15bmVpZ2hi/b3Iub3JnL2NhY2hl/L2RhdGEvaW1hZ2Uv/Z2FtZS8xMC1taW51/dGVzLXRpbGwtZGF3/bi1jb3Zlci1tMzAw/eDMwMC5wbmc'
   },
+
+  {
+  id: '!clashroyale',
+  title: 'Clash Royale (HIGHLY REQUESTED)',
+  url: 'https://www.easyfun.gg/cloud-games/clash-royale-cloud-online.html',
+  image: 'https://games.lol/wp-content/uploads/2020/12/clash-royale-free-full-version.jpg'
+  },
+  
 ]
 games.sort(function (a, b) {
   return a.title.localeCompare(b.title);
